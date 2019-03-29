5e17edf9d863cdbe0c199a106da933a381896906
Imran Sayed <imranhsayed@gmail.com>
2019-03-29 14:27:40 +0530

    Add loadble

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from 'react';
-import About from './components/About';
+import Loadable from 'react-loadable';
+
+/**
+ * The difference between import at the top and the one inside the Loadable() down below is that
+ * the import statement at the top isn't a function invocation, its a module declaration. Its static and
+ * you cannot use variable on top. So you can statically analyze your bundles. But the import() is a function.
+ * So its dynamic and will split out About component into a separate component.
+ */
+const LoadableAbout = Loadable({
+	loader: () => import( './components/About.js' )
+});
+
 
 class App extends React.Component {
 
@@ -7,7 +18,7 @@ class App extends React.Component {
 		return(
 			<div>
 				<h1>This is my React Component</h1>
-				<About/>
+
 			</div>
 		);
 	}
