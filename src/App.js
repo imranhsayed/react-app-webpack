781779caaec18800a63ca4a77d7b7036ac0dfd90
Imran Sayed <imranhsayed@gmail.com>
2019-03-29 16:52:02 +0530

    remove the loadable

--- src/App.js
+++ src/App.js
@@ -1,20 +1,5 @@
 import React from 'react';
 import { Router, Link } from "@reach/router"
-import Loadable from 'react-loadable';
-
-/**
- * The difference between import at the top and the one inside the Loadable() down below is that
- * the import statement at the top isn't a function invocation, its a module declaration. Its static and
- * you cannot use variable on top. So you can statically analyze your bundles. But the import() is a function.
- * So its dynamic and will split out About component into a separate component.
- */
-const LoadableAbout = Loadable({
-	loader: () => import( './components/About' ),
-	loading() {
-		return <h1>Loading split out code...</h1>
-	}
-});
-
 
 class App extends React.Component {
 
