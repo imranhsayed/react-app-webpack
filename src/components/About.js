84d882ad3be1f378b84e757bc12b5adf3af9b7e6
Imran Sayed <imranhsayed@gmail.com>
2019-03-29 12:47:16 +0530

    Add Click functionality to Higher Order Component

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import { HOC } from './HOC';
+import HOC from './HOC';
 
 class AboutSection extends React.Component {
-	constructor( props ) {
-		super( props );
-		this.state = {
-			count: 0
-		}
-	}
-
-	incrementCount = ( event ) => {
-		this.setState( prevState => ( { count: prevState.count + 1 } ) );
-	};
 
 	render() {
-
-		const { count } = this.state;
-
 		return(
 			<div>
 				This is About
-				<h2 onClick={ this.incrementCount }>Clicked { count } times</h2>
+				<h2 onClick={ this.props.incrementCount }>Clicked { this.props.count } times</h2>
 			</div>
 		);
 	}
 }
 
-const About = HOC( { ready: true } )( AboutSection );
+const About = HOC( AboutSection );
 export default About;
